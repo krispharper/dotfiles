e97867aef74b3780f2fea63dab5570525e915bfd
Kris Harper <kris.p.harper@gmail.com>
2014-05-01 20:16:54 -0400

    Got the panel working and cleaned up some config files

---
+++ .config/sxhkd/selectDesktop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+case "$1" in
+    "1")
+        echo 1
+        ;;
+    "2")
+        echo 4
+        ;;
+    "3")
+        echo 2
+        ;;
+    "4")
+        echo 5
+        ;;
+    "5")
+        echo 3
+        ;;
+    "6")
+        echo 6
+        ;;
+esac
